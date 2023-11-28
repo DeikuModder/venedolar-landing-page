7f4054b3c55a0f6769b4a410d0c7aa8f6c1ae50a
DeikuModder <gabrieltc555@gmail.com>
2023-11-28 02:06:36 -0400

    fix grammatic errors

--- src/Components/DownloadSection.tsx
+++ src/Components/DownloadSection.tsx
@@ -1,22 +1,30 @@
-import screenshot from '../assets/app_screenshot.webp'
+import screenshot from "../assets/app_screenshot.webp";
 
 const DownloadSection = () => {
   return (
     <section id="downloadSection">
-      <h2 id='lastH2'>Que estas esperando?</h2>
+      <h2 id="lastH2">Qué estas esperando!?</h2>
 
-      <p>Toda la información del dolar en Venezuela al alcance de tu escritorio!</p>
-      <img src={screenshot} loading='lazy' alt='venedolar-screenshot' id='app_screenshot' className='images'/>
- 
-      <a 
-      href='https://download847.mediafire.com/ch4uvsa6ah3gjjSMO5uCi9YD1_VhRclj5vgykASAJXfK8hp8P3IRP5BWOaAKD6yLT-WhLGuFKiN2AA7svHauvpmPC1vofTKq9tmWFa2KAuHzwbktsLV1wDJcM8VCm2C8nAUtfjFRYAGzC-wH_1yky80kyKVb4otgdHusy7bcO6su/8c0jvgkky6qn5p9/Venedolar_v1.0.0.rar'
-      className='button download'
+      <p>
+        Toda la información del dólar en Venezuela al alcance de tu escritorio!
+      </p>
+      <img
+        src={screenshot}
+        loading="lazy"
+        alt="venedolar-screenshot"
+        id="app_screenshot"
+        className="images"
+      />
+
+      <a
+        href="https://download847.mediafire.com/ch4uvsa6ah3gjjSMO5uCi9YD1_VhRclj5vgykASAJXfK8hp8P3IRP5BWOaAKD6yLT-WhLGuFKiN2AA7svHauvpmPC1vofTKq9tmWFa2KAuHzwbktsLV1wDJcM8VCm2C8nAUtfjFRYAGzC-wH_1yky80kyKVb4otgdHusy7bcO6su/8c0jvgkky6qn5p9/Venedolar_v1.0.0.rar"
+        className="button download"
       >
-          Descarga Venedolar!
+        Descarga Venedolar!
       </a>
       <p>*Disponible solo en Windows*</p>
     </section>
-  )
-}
+  );
+};
 
-export default DownloadSection
+export default DownloadSection;
--- src/Components/FeaturesSection.tsx
+++ src/Components/FeaturesSection.tsx
@@ -1,34 +1,61 @@
-import featureImg_1 from '../assets/features_1.webp'
-import featureImg_2 from '../assets/features_2.webp'
-import featureImg_3 from '../assets/features_3.webp'
+import featureImg_1 from "../assets/features_1.webp";
+import featureImg_2 from "../assets/features_2.webp";
+import featureImg_3 from "../assets/features_3.webp";
 
 const FeaturesSection = () => {
-
   return (
     <>
       <section className="featuresSubSection" id="featuresSection">
         <h3>Selecciona el monitor que necesites!</h3>
 
-        <p>Elige cualquiera de los 5 monitores de dolar que ofrece la aplicación, todos actualizados en tiempo real</p>
-        <img src={featureImg_3} loading='lazy' alt='monitores' id='featureImg_3' className='images'/>
-
+        <p>
+          Elige cualquiera de los 5 monitores de dólar que ofrece la aplicación,
+          todos actualizados en tiempo real.
+        </p>
+        <img
+          src={featureImg_3}
+          loading="lazy"
+          alt="monitores"
+          id="featureImg_3"
+          className="images"
+        />
       </section>
 
       <section className="featuresSubSection">
         <h3>Realiza conversiones sin salir de la app!</h3>
 
-        <p>La aplicación te ofrece una ventana para poder convertir la cantidad que necesites de dolares a bolivares, y visceversa, además te indica que monitor estas usando para la conversión</p>
-        <img src={featureImg_1} loading='lazy' alt='conversor-dolar' id='featureImg_1' className='images'/>   
+        <p>
+          La aplicación te ofrece una ventana para poder convertir la cantidad
+          que necesites de dolares a bolivares, y viceversa, además te indica
+          que monitor estas usando para la conversión.
+        </p>
+        <img
+          src={featureImg_1}
+          loading="lazy"
+          alt="conversor-dolar"
+          id="featureImg_1"
+          className="images"
+        />
       </section>
-      
+
       <section className="featuresSubSection">
         <h3>Consulta otras monedas del BCV!</h3>
 
-        <p>El BCV tiene mas equivalencias de moneda que nada mas el dolar, si necesitas saber el precio del euro, el yen, la lira turca o el rublo ruso, todas estan a tu alcance con un solo botón!</p>
-        <img src={featureImg_2} loading='lazy' alt='otras-monedas-bcv' id='featureImg_2' className='images'/>
+        <p>
+          El BCV no solo tiene la equivalencia del dólar, si necesitas saber el
+          precio del Euro, el Yen, la Lira Turca o el Rublo Ruso, todas estan a
+          tu alcance con un solo botón!
+        </p>
+        <img
+          src={featureImg_2}
+          loading="lazy"
+          alt="otras-monedas-bcv"
+          id="featureImg_2"
+          className="images"
+        />
       </section>
     </>
-  )
-}
+  );
+};
 
-export default FeaturesSection
+export default FeaturesSection;
